913a18dab9f910e11de34dd7f4f1d9c6082ac1e4
Abhinav Desai <areavolume01@gmail.com>
2026-07-22 15:36:33 -0400

    Bypass parser for onnxruntime-web in Next.js webpack config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['onnxruntime-web'],
   webpack: (config, { isServer }) => {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
       layers: true,
     };
+
+    // Exclude onnxruntime-web from being parsed/minified if it triggers parser errors
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules\/onnxruntime-web/,
+      type: 'javascript/auto',
+    });
 
     if (isServer) {
       config.externals = [...(config.externals || []), 'onnxruntime-web'];
